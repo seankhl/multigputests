cc178f689c2859540bb117cc82a14b1fb9633a42
seanlaguna <sean.laguna@gmail.com>
2016-04-25 16:27:42 -0500

    tidied up gpu info stuff, shows that not on same PCI bus

--- runner_mpi.cu
+++ runner_mpi.cu
@@ -26,56 +26,32 @@ const int Y_SZ = 2048;
 const float RANGE = 32;
 const int natoms = 1000;
 
-/* Check capability of the GPU (should be done for each card to be used) */
-void printDeviceCheck()
-{
-    int ngpus;
-    cudaGetDeviceCount(&ngpus);
-    
+void checkDevices(int ngpus) {
+    /* Check capability of the GPU 
+       (should be done for each card to be used)
+    */
     std::vector<cudaDeviceProp> gpuprops(ngpus);
-    // second argument is gpu number
-    for (int i = 0; i < ngpus; ++i) {
-        cudaGetDeviceProperties(&gpuprops[i], i);
-    }
-    
-    // check results
-    std::cout <<
-        "--------------------------------------------------------------------------------" 
-    << std::endl;
-    std::cout << "Devices: " << std::endl;
-    
     bool is_fermi = true;
     bool has_uva = true;
+    // second argument is gpu number
     for (int i = 0; i < ngpus; ++i) {
-        std::cout << "    " <<
-                     gpuprops[i].name << " " <<
-                     gpuprops[i].major << " " <<
-                     gpuprops[i].unifiedAddressing << 
-                     gpuprops[i].pciBusID << " " <<
-                     gpuprops[i].pciDeviceID << std::endl;
-        is_fermi &= (gpuprops[i].major >= 2);  // must be fermi or newer
-        has_uva &= (gpuprops[i].unifiedAddressing);
+        cudaGetDeviceProperties(&gpuprops[i], i + 1);
+    }
+    // check results
+    for (int i = 0; i < ngpus; ++i) {
+        is_fermi &= (gpuprops[i].major >= 2); // must be Fermi based
+        has_uva &= (gpuprops[i].unifiedAddressing); // must be Fermi based
     }
     
     // TODO: only works for ngpus == 2
-    int access1from0;
-    int access0from1;
-    cudaDeviceCanAccessPeer(&access1from0, 1, 0);
-    cudaDeviceCanAccessPeer(&access0from1, 0, 1);
-    bool same_complex = (access1from0 && access0from1);
-    
-    std::cout << "Peer access: " << std::endl << 
-                 "    access 1 from 0: " << access1from0 << std::endl <<
-                 "    access 0 from 1: " << access0from1 << std::endl;
-    
-    std::cout << "General info: " << std::endl << 
-                 "    num devices? " << ngpus << std::endl <<
-                 "    is fermi? " << is_fermi << std::endl <<
-                 "    has uva? " << has_uva << std::endl <<
-                 "    same complex? " << same_complex << std::endl;
-    std::cout <<
-        "--------------------------------------------------------------------------------" 
-    << std::endl;
+    int access2from1, access1from2;
+    cudaDeviceCanAccessPeer(&access2from1, 2, 1);
+    cudaDeviceCanAccessPeer(&access1from2, 1, 2);
+    bool same_complex = (access2from1 && access1from2);
+    std::cout << "num devices: " << ngpus << 
+                 "; is fermi? " << is_fermi << 
+                 "; has uva? " << has_uva <<
+                 "; same complex? " << same_complex << std::endl;
 }
 
 int run(int ngpus_in, int nt)
@@ -86,7 +62,9 @@ int run(int ngpus_in, int nt)
 
     int ngpus;
     cudaGetDeviceCount(&ngpus);
+
     if (ngpus_in < ngpus) { ngpus = ngpus_in; }
+    checkDevices(ngpus);
 
     // random initial values for atoms
     std::random_device rd;
@@ -161,7 +139,7 @@ int run(int ngpus_in, int nt)
     std::vector<float4 *> atoms_old_dev(ngpus);
     std::vector<float4 *> atoms_new_dev(ngpus);
     for (int i = 0; i < ngpus; ++i) {
-        cudaSetDevice(i);
+        cudaSetDevice(i + 1);
         
         // new vals: before timestepping, current vals always in here
         HANDLE_ERROR( cudaMalloc((void **)&atoms_new_dev[i], 
@@ -179,6 +157,7 @@ int run(int ngpus_in, int nt)
                                  atoms_width[i] * sizeof(float4), 
                                  cudaMemcpyHostToDevice) );
         
+        
         atoms_off += atoms_width[i];
     }
     
@@ -186,14 +165,14 @@ int run(int ngpus_in, int nt)
     std::vector<float4 *> ghost_hi_dev(ngpus-1);
     for (int i = 0; i < ngpus-1; ++i) {  // don't need last split, always end
         // ghost vals
-        cudaSetDevice(i + 1);  // next proc gets lo ghosts
+        cudaSetDevice(i + 2);  // next proc gets lo ghosts
         HANDLE_ERROR( cudaMalloc((void **)&ghost_lo_dev[i], 
                                  (atoms_split[i] - cutlo[i]) * sizeof(float4)) );
         HANDLE_ERROR( cudaMemcpy((void *)ghost_lo_dev[i], 
                                  (void *)(atoms.data() + cutlo[i]), 
                                  (atoms_split[i] - cutlo[i]) * sizeof(float4), 
                                  cudaMemcpyHostToDevice) );
-        cudaSetDevice(i);  // this proc gets hi ghosts
+        cudaSetDevice(i + 1);  // this proc gets hi ghosts
         HANDLE_ERROR( cudaMalloc((void **)&ghost_hi_dev[i], 
                                  (cuthi[i] - atoms_split[i]) * sizeof(float4)) );
         HANDLE_ERROR( cudaMemcpy((void *)ghost_hi_dev[i], 
@@ -219,7 +198,7 @@ int run(int ngpus_in, int nt)
     float4 *atoms_tmp_dev;
     for (int t = 0; t < nt; ++t) {
         for (int i = 0; i < ngpus; ++i) {
-            cudaSetDevice(i);
+            cudaSetDevice(i + 1);
             //if (t % 10000 == 0) { std::cout << t << std::endl; }
 
             // figure out what our needed ghosts are
@@ -298,7 +277,7 @@ int run(int ngpus_in, int nt)
 
     atoms_off = 0;
     for (int i = 0; i < ngpus; ++i) {
-        cudaSetDevice(i);
+        cudaSetDevice(i + 1);
         HANDLE_ERROR( cudaMemcpy((void *)(atoms.data() + atoms_off), 
                                  (void *)atoms_new_dev[i], 
                                  atoms_width[i] * sizeof(float4), 
@@ -323,27 +302,18 @@ int run(int ngpus_in, int nt)
     
 }
 
-int main()
-{
-    printDeviceCheck();
-
-    std::chrono::time_point<std::chrono::steady_clock> two_start, two_end;
-    two_start = std::chrono::steady_clock::now();
-    run(2, 10);
-    two_end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> two_dur = two_end - two_start;
-    std::cout <<
-        "--------------------------------------------------------------------------------" 
-    << std::endl;
-    
+int main() {
     std::chrono::time_point<std::chrono::steady_clock> one_start, one_end;
     one_start = std::chrono::steady_clock::now();
-    run(1, 10);
+    run(2, 10);
     one_end = std::chrono::steady_clock::now();
     std::chrono::duration<double> one_dur = one_end - one_start;
-    std::cout <<
-        "--------------------------------------------------------------------------------" 
-    << std::endl;
+    
+    std::chrono::time_point<std::chrono::steady_clock> two_start, two_end;
+    two_start = std::chrono::steady_clock::now();
+    run(1, 10);
+    two_end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> two_dur = two_end - two_start;
     
     std::cout << "one took: " << one_dur.count() << " seconds; " << std::endl;
     std::cout << "two took: " << two_dur.count() << " seconds; " << std::endl;
